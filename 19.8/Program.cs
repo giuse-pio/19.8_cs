033df1f71799b5d71fa7e0c6357c9706a7bf5f16
giupe <giupe@MSI>
2025-09-19 18:41:33 +0200

    Add project files.

---
+++ 19.8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _19._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> voti = new List<float>();
+            float voto;
+            Console.WriteLine("Inserisci i voti (da 0 a 10), per uscire premere INVIO");
+            while (float.TryParse(Console.ReadLine(), out voto))
+            {
+                if (voto < 0 || voto > 10)
+                    break;
+                voti.Add(voto);
+            }
+            float media = 0;
+            media = voti.Average();
+            int count = 0;
+            foreach (float v in voti)
+            {
+                if (v < media)
+                    count++;
+            }
+            Console.WriteLine($"Media: {media}, Voti sotto la media: {count}");
+        }
+    }
+}
